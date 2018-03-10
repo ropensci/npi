daceb1fdf3726971470e7e55f2b736a01cc83415
Frank Farach <frank.farach@gmail.com>
2018-03-10 00:56:40 -0800

    Added test for empty list result

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -9,3 +9,7 @@ test_that("search_npi returns error messages from API", {
   expect_message(search_npi(provider_type = 1),
                  "Error 09 - enumeration_type requires additional search criteria")
 })
+
+test_that("search_npi returns an empty list when there are no results", {
+  expect_identical(search_npi(postal_code = "zzz"), list())
+})
