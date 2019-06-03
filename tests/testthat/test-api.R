81519ae65d4cece7f3c4bffe220574c64e6acc19
Frank Farach <frank.farach@gmail.com>
2019-06-03 12:14:59 -0700

    Add test for multi-line error message

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -4,6 +4,8 @@ test_that("search_npi provides messages when provider_type value is incorrect", 
   expect_message(search_npi(provider_type = "NPI1"))
 })
 
-test_that("search_npi returns error messages from API", {
-  expect_error(search_npi(provider_type = 1))
+multi_error <- "field state requires additional search criteria\nError: enumeration_type requires additional search criteria"
+
+test_that("search_npi returns multi-line error messages from API", {
+  expect_error(search_npi(provider_type = 1, state = "CA"), multi_error)
 })
